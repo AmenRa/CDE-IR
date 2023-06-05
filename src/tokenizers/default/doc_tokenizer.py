848a094fd2fce49b478421127d187373e9f1e8ef
AmenRa <elias.bssn@gmail.com>
2023-06-05 21:12:59 +0200

    Formatting

--- src/tokenizers/default/doc_tokenizer.py
+++ src/tokenizers/default/doc_tokenizer.py
@@ -8,18 +8,14 @@ class DocTokenizer:
         max_len: int = 512,
         add_prefix_token: bool = True,
     ):
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            language_model, use_fast=True
-        )
+        self.tokenizer = AutoTokenizer.from_pretrained(language_model, use_fast=True)
 
         self.max_len = max_len
 
         self.add_prefix_token = add_prefix_token
         self.prefix_token = "[unused1]"
         self.prefix_token_id = self.tokenizer.convert_tokens_to_ids("[unused1]")
-        self.tokenizer.add_special_tokens(
-            {"additional_special_tokens": ["[unused1]"]}
-        )
+        self.tokenizer.add_special_tokens({"additional_special_tokens": ["[unused1]"]})
 
     def __call__(self, texts: list[str]) -> dict:
         if self.add_prefix_token:
--- src/tokenizers/default/query_tokenizer.py
+++ src/tokenizers/default/query_tokenizer.py
@@ -8,18 +8,14 @@ class QueryTokenizer:
         max_len: int = 64,
         add_prefix_token: bool = True,
     ):
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            language_model, use_fast=True
-        )
+        self.tokenizer = AutoTokenizer.from_pretrained(language_model, use_fast=True)
 
         self.max_len = max_len
 
         self.add_prefix_token = add_prefix_token
         self.prefix_token = "[unused0]"
         self.prefix_token_id = self.tokenizer.convert_tokens_to_ids("[unused0]")
-        self.tokenizer.add_special_tokens(
-            {"additional_special_tokens": ["[unused0]"]}
-        )
+        self.tokenizer.add_special_tokens({"additional_special_tokens": ["[unused0]"]})
 
     def __call__(self, texts: list[str]) -> dict:
         if self.add_prefix_token:
